77ee8ea76e4829388d20b80a1edfb8e728e0013d
kylasanders1223-wq <kylasanders1223@gmail.com>
2026-07-11 15:39:35 -0400

    Correct syntax and enhance user prompts in LetterSequence.sh
    Fix syntax errors and improve game instructions.

--- LetterSequence.sh
+++ LetterSequence.sh
@@ -4,3 +4,76 @@
 
 #Make sure to record your thoughts and instructions in your OWN words in the comments 
 #and to use the comments to show that you can explain your code.
+echo "You've entered the DNA sequence game."
+echo "Your goal: to memorize a DNA nucleotide sequence given to you one letter at a time (A, T, C,G) until you get five of the same letter in a row."
+echo "If you enter the sequence incorrectly, you will be required to start over."
+
+# The computer can chose from these letters
+letters=("A" "T" "C" "G")
+
+win_length=5
+round=1
+
+while true
+do
+  echo "Round $round"
+  echo "Get ready..."
+    sleep 1
+    printf "\n\n\n"
+
+  # picking random letter
+  random_index=$((random % 4)
+  new_letter="${letters[random_index]}"
+
+  # Add the new DNA letter
+  sequence="${sequence}${new_letter}"
+
+  for (( i=0; i<${#sequence}; i++ ))
+  do
+        current_letter="${sequence:$i:1}"
+  esac
+
+  echo "DNA Letter: $current_letter"
+        echo "Base Name: $base_name"
+        sleep 1
+        printf "\n\n"
+        
+  done
+    
+   echo "Your turn! Type the sequence and press ENTER:"
+    read -r USER_INPUT
+
+# make answer caps
+USER_INPUTCORRECTED="$USER_INPUT^^"
+
+
+   if [ "$USER_INPUTCORRECTED" = "$SEQUENCE" ]
+   then
+        echo "\nCORRECT! Your sequence alignment is correct."
+         echo "Current sequence length: ${#sequence}"
+        CURRENT_ROUND=$(( round + 1 ))
+   else echo "Not quite! Try again!"
+
+     if [[ ${#sequence} -ge $win_length ]]
+        then
+            printf "\n\n\n"
+            echo "You correctly repeated a DNA sequence of ${#sequence} ."
+            echo "Final DNA sequence: $sequence"
+            echo "You're a master scientist!"
+            break
+        fi
+
+        round=$((round + 1))
+
+      else
+          echo
+          echo "Incorrect."
+          echo "You typed: $USER_INPUTCORRECT"
+          echo "The correct DNA sequence was: $sequence"
+          echo
+          echo "TRY AGAIN."
+          break
+      fi
+
+done
+        
